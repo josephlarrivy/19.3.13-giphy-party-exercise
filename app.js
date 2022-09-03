5c2b7b43a3f83e9595921b36d75fd0eb0edd4dde
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-03 10:37:58 -0500

    initial html, css, js setup complete

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Let's get this party started!");
+const submitBtn = document.getElementById('submitbtn');
+const removeBtn = document.getElementById('removebtn');
+const inputText = document.getElementById('inputtext');
+
+function getSearchValue () {
+    console.log(inputText.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    getSearchValue();
+    inputText.value = '';
+});
+
+// removeBtn.addEventListener('click', function() {
+
+// })
