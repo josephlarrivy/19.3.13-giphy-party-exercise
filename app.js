29e20ef607c885cc5878f8fafe8c8313bba7bd0f
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-03 12:25:56 -0500

    todo: retreive url from api data

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
+//giphy key: 51m0wO8gYEdB7dx7kJBzsdzMs79X5WFp
 const submitBtn = document.getElementById('submitbtn');
 const removeBtn = document.getElementById('removebtn');
 const inputText = document.getElementById('inputtext');
+const gifDiv = document.getElementById('gifdiv');
 
-function getSearchValue () {
-    console.log(inputText.value);
+async function getData () {
+    const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { 
+        q: inputText.value,
+        api_key: '51m0wO8gYEdB7dx7kJBzsdzMs79X5WFp' 
+    }});
+
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('gifs');
+    
+    
+
+    
+    //newDiv.setAttribute.style.backgroundImage = `url(${response.data})`;
+    console.log(response.data[0].url)
+    //^^need to extract url from returned data
+
+
+
+
+
+
+    gifDiv.append(newDiv);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    getSearchValue();
+    getData();
     inputText.value = '';
 });
 
